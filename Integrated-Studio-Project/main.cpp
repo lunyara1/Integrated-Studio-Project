1fb9fac97a9a4a4476030a8e2991b3b08bcbaca6
Vasyl <tronadello@gmail.com>
2022-08-23 11:28:02 +1200

    Initial Commit

---
+++ Integrated-Studio-Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Integrated-Studio-Project
+//
+//  Created by Vasyl Kolomiiets on 23/08/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
