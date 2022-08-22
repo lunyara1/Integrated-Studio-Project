8fb252f90afb69560c6d4a870ae4ae06cb6598a9
Nataliia Kolomiiets <111809774+Nata1iia@users.noreply.github.com>
2022-08-23 11:48:44 +1200

    Update main.cpp

--- Integrated-Studio-Project/main.cpp
+++ Integrated-Studio-Project/main.cpp
@@ -11,4 +11,40 @@ int policyNumber;
 double premium;
 
 public:
+Insurance()
+{
+custName="";
+policyNumber=0;
+premium=0.0;
 }
+Insurance(string cn,int pn,double prem)
+{
+custName = cn;
+policyNumber=pn;
+premium=prem;
+}
+string getCustomerName()
+{
+return custName;
+}
+void setCustomerName(string name)
+{
+custName = name;
+}
+int getPolicyNumber()
+{
+return policyNumber;
+}
+void setPolicyNumber(int pn)
+{
+policyNumber = pn;
+}
+double getPremiumValue()
+{
+return premium;
+}
+void setPremium(double prem)
+{
+premium = prem;
+}  
+
